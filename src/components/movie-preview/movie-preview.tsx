1d30c87864d0a9d73ad0700c708fd2aeba26459d
robert0203 <reifschneider0203@gmail.com>
2022-05-04 12:43:07 +0200

    Farbfelder eingefügt; height stimmt noch nicht

--- src/components/movie-preview/movie-preview.tsx
+++ src/components/movie-preview/movie-preview.tsx
@@ -13,46 +13,54 @@ export class MoviePreview {
         <div class="container">
           <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
             <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
-            </div>
-            <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
-            </div>
-            <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
-            </div>
-            <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
-            </div>
-            <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
+              <div class="p-3">
+                <div class="feld"></div>
+              </div>
             </div>
           </div>
         </div>
