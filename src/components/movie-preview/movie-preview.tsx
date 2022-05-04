df6692ada3733b6c4748e2e3e513b04c428c4b93
robert0203 <reifschneider0203@gmail.com>
2022-05-04 14:46:21 +0200

    Responsibilty angepasst

--- src/components/movie-preview/movie-preview.tsx
+++ src/components/movie-preview/movie-preview.tsx
@@ -12,52 +12,52 @@ export class MoviePreview {
       <Host>
         <div class="container">
           <div class="row g-2 g-lg-3">
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
             </div>
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
             </div>
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
             </div>
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
             </div>
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
             </div>
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
             </div>
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
             </div>
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
             </div>
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
             </div>
-            <div class="col col-4 col-lg-3">
+            <div class="col-12 col-sm-6 col-lg-3 col-xl-2">
               <div class="p-3">
                 <div class="feld"></div>
               </div>
